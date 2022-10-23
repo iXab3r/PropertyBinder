d94a1988ed64663acb2a656e68ab629cb4f1af9c
Xab3r <mail.xaber@gmail.com>
2022-10-23 12:26:12 +0200

    Fixed a bug in BindableMember - property selector was not working in some cases for inherited classes

---
+++ PropertyBinder/Engine/BindingNodeRootBuilder.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace PropertyBinder.Engine;
+
+internal sealed class BindingNodeRootBuilder<TContext> : BindingNodeBuilder<TContext, TContext>
+{
+    private BindingNodeRootBuilder(IDictionary<string, IBindingNodeBuilder<TContext>> subNodes, IDictionary<string, List<int>> bindingActions, ICollectionBindingNodeBuilder<TContext> collectionNode)
+        : base(_ => _, subNodes, bindingActions, collectionNode)
+    {
+    }
+
+    public BindingNodeRootBuilder()
+        : base(_ => _)
+    {
+    }
+
+    public override IBindingNodeBuilder<TNewParent> CloneForDerivedParentType<TNewParent>()
+    {
+        return new BindingNodeRootBuilder<TNewParent>(
+            SubNodes?.ToDictionary(x => x.Key, x => x.Value.CloneForDerivedParentType<TNewParent>()),
+            BindingActions.ToDictionary(x => x.Key, x => new List<int>(x.Value)),
+            CollectionNode?.CloneForDerivedParentType<TNewParent>());
+    }
+}
